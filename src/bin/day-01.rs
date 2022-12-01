a307984f6a1a308377257d78ef24bbb9e4a82df8
Nicholas Hollett <niax@niax.co.uk>
2022-11-30 21:55:44 -0800

    Update to new things

--- src/bin/day-01.rs
+++ src/bin/day-01.rs
@@ -3,8 +3,8 @@ use aoc2022::commons::io::load_argv_records;
 fn main() {
     let ints: Vec<Vec<u32>> = load_argv_records("").map(|res| res.unwrap()).collect();
     let mut sums: Vec<u32> = ints.iter().map(|v| v.iter().sum()).collect();
-    sums.sort_unstable();
+    sums.sort_unstable_by(|a, b| b.cmp(a));
 
-    println!("{:?}", sums.iter().last().unwrap());
-    println!("{:?}", sums.iter().rev().take(3).sum::<u32>());
+    println!("{:?}", sums[0]);
+    println!("{:?}", sums[0..3].iter().sum::<u32>());
 }

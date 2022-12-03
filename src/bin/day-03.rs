99a95c433d24dcb692abb48527e273de8b221dd1
Nicholas Hollett <niax@niax.co.uk>
2022-12-03 11:49:52 -0800

    Clean up day 3, with proper error handling

--- src/bin/day-03.rs
+++ src/bin/day-03.rs
@@ -1,62 +1,70 @@
+use thiserror::Error;
 use aoc2022::commons::io::load_argv_lines;
-use std::collections::HashMap;
 use std::collections::HashSet;
+use std::error::Error;
+use std::str::FromStr;
 
-fn priority_for_char(ch: char) -> usize {
+#[derive(Debug, Error, Hash, Eq, PartialEq)]
+enum ParseError {
+    #[error("Bad item")]
+    BadItem,
+}
+
+#[derive(Debug, Error, Hash, Eq, PartialEq)]
+enum RunError {
+    #[error("No matches")]
+    NoMatch,
+}
+
+fn priority_for_char(ch: char) -> Result<usize, ParseError> {
     let c = ch as usize;
     if (65..92).contains(&c) {
-        c - 64 + 26
+        Ok(c - 64 + 26)
     } else if (97..123).contains(&c) {
-        c - 96
+        Ok(c - 96)
     } else {
-        panic!("Bad priority: {}", c);
+        Err(ParseError::BadItem)
     }
 }
 
-fn main() {
-    let input: Vec<String> = load_argv_lines().map(|res| res.unwrap()).collect();
+#[derive(Debug)]
+struct Bag {
+    c1: HashSet<usize>,
+    c2: HashSet<usize>,
+    all: HashSet<usize>,
+}
 
-    let mut part1 = 0;
-    let mut part2 = 0;
-    let mut group_items = HashMap::new();
-    for (i, backpack) in input.iter().enumerate() {
-        let compartment_size = backpack.len() / 2;
-        let c1_range = ..compartment_size;
-
-        let mut c1 = HashSet::with_capacity(128);
-        let mut c2 = HashSet::with_capacity(128);
-        let mut combined = HashSet::with_capacity(128);
-        for (j, ch) in backpack.chars().enumerate() {
-            let priority = priority_for_char(ch);
-            if c1_range.contains(&j) {
-                c1.insert(priority);
-            } else {
-                c2.insert(priority);
-            };
-            combined.insert(priority);
-        }
-
-        for c in combined {
-            let entry = group_items.entry(c).or_insert(0);
-            *entry += 1;
-        }
-
-        let mut inter = c1.intersection(&c2);
-        let common = inter.next().unwrap();
-        assert_eq!(inter.next(), None);
-
-        part1 += common;
-
-        if i % 3 == 2 {
-            for (priority, count) in &group_items {
-                if *count == 3 {
-                    part2 += priority;
-                }
-            }
-            group_items.clear();
-        }
+impl Bag {
+    pub fn badge(&self) -> Option<usize> {
+        let mut inter = self.c1.intersection(&self.c2);
+        inter.next().copied()
     }
+}
+
+impl FromStr for Bag {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (s1, s2) = s.split_at(s.len() / 2);
+        Ok(Bag {
+            c1: s1.chars().map(|c| priority_for_char(c)).collect::<Result<_, _>>()?,
+            c2: s2.chars().map(|c| priority_for_char(c)).collect::<Result<_, _>>()?,
+            all: s.chars().map(|c| priority_for_char(c)).collect::<Result<_, _>>()?,
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input: Vec<Bag> = load_argv_lines().map(|res| res).collect::<Result<_, _>>()?;
+
+    let part1: usize = input.iter().map(|b| b.badge().unwrap()).sum();
+    let part2: usize = input.chunks_exact(3).map(|window| {
+        window[0].all.intersection(&window[1].all).find(|priority| window[2].all.contains(priority)).ok_or(RunError::NoMatch)
+    }).sum::<Result<_, _>>()?;
+
 
     println!("{}", part1);
     println!("{}", part2);
+
+    Ok(())
 }
